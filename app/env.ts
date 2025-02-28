a773e7738dbb517797273745dac1a1aadd3488d4
Chanakan Moongthin <chanakan5591@chanakancloud.net>
2025-02-28 21:04:04 +0700

    removed clerk stuff

--- app/env.ts
+++ app/env.ts
@@ -5,11 +5,9 @@ export const env = createEnv({
   server: {
     TURSO_DATABASE_URL: z.string().url().startsWith("libsql://"),
     TURSO_AUTH_TOKEN: z.string(),
-    CLERK_SECRET_KEY: z.string().startsWith('sk_')
   },
   clientPrefix: "VITE_",
   client: {
-    VITE_CLERK_PUBLISHABLE_KEY: z.string().startsWith('pk_'),
   },
 
   runtimeEnv: process.env,
