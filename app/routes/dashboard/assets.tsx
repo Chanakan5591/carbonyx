ce8d70ccef8a539957135c100f2bbefea63fc6eb
Chanakan Moongthin <chanakan5591@chanakancloud.net>
2025-03-29 08:40:15 +0700

    added a disclaimer to assets management page that it's not done

--- app/routes/dashboard/assets.tsx
+++ app/routes/dashboard/assets.tsx
@@ -20,6 +20,7 @@ export default function Assets() {
         })}
       >
         Assets Management
+        We are working hard to get this going, but it's going to be great! In the meantime, please check out manual emissions pages for the current organization carbon emissions information
       </span>
 
     </div>
